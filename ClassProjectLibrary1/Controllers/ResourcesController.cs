7d13662657a846d0270af4ae1f5190ad62a8ce1b
Levi Tracy <LTracy@tql.com>
2022-05-20 12:21:58 -0400

    Folder and Resources add

---
+++ ClassProjectLibrary1/Controllers/ResourcesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProjectLibrary1
+    {
+    public class ResourcesController
+        {
+
+
+        private AppDbContext _context = new AppDbContext();
+
+
+        #region GetUsers()
+        public List<Resources> GetResources()
+            {
+            return _context.Resources.ToList();
+            }
+        #endregion
+
+        #region GetUser()
+        public Resources GetResource(int id)
+            {
+            return _context.Resources.Find(id);
+            }
+        #endregion
+
+
+        #region AddUser()
+        public Resources AddUser(Resources resources)
+            {
+            _context.Resources.Add(resources);
+            var rowsAffected = _context.SaveChanges();
+            if (rowsAffected != 1)
+                {
+                throw new Exception("AddUser Failed!");
+                }
+            return resources;
+            }
+        #endregion
+
+        #region UpdateUser()
+        public void UpdateResources(Resources resources)
+            {
+            _context.Entry(resources).State = EntityState.Modified;
+            var rowsAffected = _context.SaveChanges();
+            if (rowsAffected != 1)
+                {
+                throw new Exception("UpdateUser Failed");
+                }
+            }
+        #endregion
+
+
+        #region DeleteUser()
+        public void DeleteResources(int id)
+            {
+            var user = GetResource(id);
+            if (user is null)
+                {
+                throw new Exception("User not found!");
+                }
+            _context.Resources.Remove(user);
+            var rowsAffected = _context.SaveChanges();
+            if (rowsAffected != 1)
+                {
+                throw new Exception("DeleteUser Failed");
+                }
+            }
+        #endregion
+        }
+    }
