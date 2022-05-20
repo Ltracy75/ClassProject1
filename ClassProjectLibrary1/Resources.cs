9cbf5051f00de21c830e529d2e315f33b6918053
Rebecca Owens <ROwens@tql.com>
2022-05-20 11:04:01 -0400

    initial commit

---
+++ ClassProjectLibrary1/Resources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProjectLibrary1
+{
+    internal class Resources
+    {
+        public int Id { get; set; }
+        [StringLength(30)]
+        public string Name { get; set; } = string.Empty;
+        public int HoursPerDay { get; set; }
+
+        public int ProjectId { get; set; }
+        public virtual Projects? Projects { get; set; }
+    }
+}
