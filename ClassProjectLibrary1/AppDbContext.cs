c943bfa3a401ec2a175582efc08aca20091dbbff
Levi Tracy <LTracy@tql.com>
2022-05-20 11:20:35 -0400

    Updated,added DbContext

---
+++ ClassProjectLibrary1/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProjectLibrary1
+    {
+    internal class AppDbContext : DbContext
+        {
+        AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Projects> Projects { get; set; }
+
+        public DbSet<Resources> Resources { get; set; }
+        public DbSet<Work> Works { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+        }
+    }
--- ClassProjectLibrary1/Work.cs
+++ ClassProjectLibrary1/Work.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace GroupProjectLibrary
+namespace ClassProjectLibrary1
     {
     public class Work
         {
